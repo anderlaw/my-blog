10b8c81419ade293a5e3ec7153b22328d07d423d
hajerbin <974088545@qq.com>
2019-09-30 16:45:03 +0800

    修改导航布局

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="nav-list" @click="handleNavClick">
-      <div :class="`item ${isActive('all')}`" tag="all">全部文章</div>
-      <div :class="`item ${isActive('tech')}`" tag="tech">技术博客</div>
-      <div :class="`item ${isActive('travel')}`" tag="travel">旅行日记</div>
-      <div class="item" tag="contact">联系</div>
-    </div>
-    <div class="contact-me" v-show="contactVisible">
-      <span class="close" @click="contactVisible = false;">&times;</span>
-      <div class="title">联系博主:</div>
-      <div>
-        <span>Email:</span>
-        <span>hajerbin@foxmail.com</span>
+<!-- 需要让绝对定位元素继承相对元素的宽度：100%，fixed会继承body的宽。 -->
+  <div style="position:relative;">
+    <div class="nav-wrapper">
+      <div class="nav-list" @click="handleNavClick">
+        <div :class="`item ${isActive('all')}`" tag="all">全部文章</div>
+        <div :class="`item ${isActive('tech')}`" tag="tech">技术博客</div>
+        <div :class="`item ${isActive('travel')}`" tag="travel">旅行日记</div>
+        <div class="item" tag="contact">联系</div>
+      </div>
+      <div class="contact-me" v-show="contactVisible">
+        <span class="close" @click="contactVisible = false;">&times;</span>
+        <div class="title">联系博主:</div>
+        <div>
+          <span>Email:</span>
+          <span>hajerbin@foxmail.com</span>
+        </div>
       </div>
     </div>
   </div>
@@ -22,46 +25,46 @@ export default {
   data() {
     return {
       articleList: [],
-      contactVisible:false
+      contactVisible: false
     };
   },
   methods: {
     handleNavClick(ev) {
       let tag = ev.target.getAttribute("tag");
-      if(tag === 'contact'){
+      if (tag === "contact") {
         this.contactVisible = true;
-      }else{
-        this.$store.commit('update_articleType',tag);
+      } else {
+        this.$store.commit("update_articleType", tag);
         this.$router.push({
-          path:'/',
-          query:{
-            type:tag
+          path: "/",
+          query: {
+            type: tag
           }
         });
       }
     },
     isActive(tag) {
-      return this.$store.state.articleType === tag ? 'active':'';
+      return this.$store.state.articleType === tag ? "active" : "";
     }
   },
-  mounted(){
+  mounted() {
     //update store base on path.query
     var path = this.$route.path;
-    var type = this.$route.query.type || 'all';
-    if(path === '/'){
-      this.$store.commit('update_articleType',type)
+    var type = this.$route.query.type || "all";
+    if (path === "/") {
+      this.$store.commit("update_articleType", type);
     }
-    
+
     //设置固定
-    var height = document.querySelector('.nav-wrapper').clientHeight;
-    document.querySelector('#app').style.paddingTop = height + 'px';
+    var height = document.querySelector(".nav-wrapper").clientHeight;
+    document.querySelector("#app").style.paddingTop = height + "px";
   }
 };
 </script>
 
 <style scoped lang="scss">
 .nav-wrapper {
-  position: fixed;
+  position: absolute;
   top: 0;
   width: 100%;
   background-color: #fff;
@@ -70,14 +73,14 @@ export default {
   font-size: 16px;
   line-height: 1.6;
 }
-.contact-me{
-  .close{
+.contact-me {
+  .close {
     font-size: 20px;
     padding: 4px;
     cursor: pointer;
     float: right;
   }
-  .title{
+  .title {
     font-size: 18px;
     font-weight: bold;
     margin: 4px 0;
